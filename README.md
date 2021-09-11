9e20e3637ca60b771cd6c6b74d860a3c7c85f94c
Jason Florentino <jasonmflorentino@gmail.com>
2021-09-10 20:04:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ✂️ Rock, Paper, Scissor in the Console
+
+Been in the browser console a lot lately and I was thinking last night that it might be fun to write some code that runs a game in there. No webpage, or fancy styling, just JavaScript. So here it is. (I only wrote it in a matter of minutes so please don't judge.)
+
+— Jason, September 10, 2021
+
+## But Why
+I feel like everyone goes through the practice of making a Rock, Paper, Scissors game when they start out programming, and that's who I hope can get something out of this. There's a million ways you could write this game, and here's one more. Now there's some wacky stuff in here that you'd never do "in real life", but I think this is a fun toy that brings in some neat tools and concepts with literally zero barrier for entry – You've already got a web browser, here's some code you can run right away.† Once you've played the game (or before!), have a look through and try to follow the paths of the program, and why I might've done things this way or that way, or how you might do them differently.
+
+## How to use this
+- Open a new tab in your browser
+- Open the developer tools panel: View > Developer > Developer Tools (On Chrome)
+- Go to the JavaScript console, if it's not already open: 
+  - Select the 'Console' tab at the top.
+  - There's a chance it might be hidden in a *see more* menu. It looks like this `>>` in Chrome.
+  - You should see a blank area with a prompt (`> `) and your typical blinking text cursor
+    ```
+    > you can type in there|
+    ```
+- Paste the contents of `game.js` in there and hit `ENTER`†
+- "Call" the function that starts the game: 
+  - Enter this into JS console:
+    ```javascript
+    playRockPaperScissors();
+    ```
+- Enjoy
+
+## ⚠️ Note
+† In general it's probably **not** a great idea to paste code into your console unless you know what it does. But there's no funny business in here, I promise!
---
+++ game.js
@@ -0,0 +1,128 @@
+// Game variables
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+const QUIT = "quit";
+const PLAYER = "player";
+const COMPUTER = "computer";
+const TIE = "tie";
+let PLAYER_SCORE = 0;
+let COMPUTER_SCORE = 0;
+
+// Game functions
+
+/**
+ * Determines if given word is a legal input
+ * @returns `true` if given word is valid, `false` otherwise
+ */
+function isLegalChoice(word) {
+  const legalChoices = [ROCK, PAPER, SCISSORS, QUIT];
+  return legalChoices.includes(word);
+}
+
+/**
+ * Asks the player what their choice is.
+ * @returns `string` constant of player's choice
+ */
+function getPlayerChoice() {
+  let choice;
+  while (true) {
+    choice = prompt("Rock, Paper, or Scissors? (or quit to exit)", "")
+      .toLowerCase()
+      .trim();
+    if (isLegalChoice(choice)) break;
+    alert(
+      `I don't know "${choice}"\nPlease enter: rock, paper, scissors, or quit.`
+    );
+  }
+  switch (choice) {
+    case ROCK:
+      return ROCK;
+    case PAPER:
+      return PAPER;
+    case SCISSORS:
+      return SCISSORS;
+    case QUIT:
+      return QUIT;
+    default:
+      console.error("Unknown case");
+      return QUIT;
+  }
+}
+
+/**
+ * Gets a random choice for the computer
+ * @returns `string` constant of the computer's choice
+ */
+function getComputerChoice() {
+  const choices = [ROCK, PAPER, SCISSORS];
+  const randIdx = Math.round(Math.random() * (choices.length - 1));
+  return choices[randIdx];
+}
+
+/**
+ * Displays a message to user before returning false
+ * @returns `false`
+ */
+function quitGame() {
+  alert("Quiting game...");
+  return false;
+}
+
+/**
+ * Determines the result of the round.
+ * @returns `string` constant of the winner
+ */
+function determineWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) return TIE;
+  if (playerChoice === ROCK) {
+    if (computerChoice === SCISSORS) return PLAYER;
+    if (computerChoice === PAPER) return COMPUTER;
+  }
+  if (playerChoice === PAPER) {
+    if (computerChoice === SCISSORS) return COMPUTER;
+    if (computerChoice === ROCK) return PLAYER;
+  }
+  if (playerChoice === SCISSORS) {
+    if (computerChoice === ROCK) return COMPUTER;
+    if (computerChoice === PAPER) return PLAYER;
+  }
+}
+
+/**
+ * Handles displaying message to the user and
+ * incrementing the scores at the end of the round.
+ */
+function handleWinner(winner, playerChoice, computerChoice) {
+  let msg = "";
+  if (winner === PLAYER) {
+    msg += "🎉 You won!";
+    PLAYER_SCORE++;
+  }
+  if (winner === COMPUTER) {
+    msg += "😢 You lost!";
+    COMPUTER_SCORE++;
+  }
+  if (winner === TIE) msg += "😲 It's a tie!";
+  msg += `\nYou chose ${playerChoice}, the computer chose ${computerChoice}.`;
+  msg += `\n\nThe score is now:\nYou: ${PLAYER_SCORE} – Computer: ${COMPUTER_SCORE}`;
+  alert(msg);
+}
+
+/**
+ * Handles playing one round of Rock, Paper, Scissors.
+ * @returns `true` if play should continue, `false` to quit the game.
+ */
+function playRound() {
+  const playerChoice = getPlayerChoice();
+  if (playerChoice === QUIT) return quitGame();
+  const computerChoice = getComputerChoice();
+  const winner = determineWinner(playerChoice, computerChoice);
+  handleWinner(winner, playerChoice, computerChoice);
+  return true;
+}
+
+// Call this to play game
+function playRockPaperScissors() {
+  while (playRound()) {}
+}
